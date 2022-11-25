346fcf33601d8f373e6bd41f7cb1461f9832ad57
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-11-26 03:56:21 +0600

    mongoconnect

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,6 +9,45 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.de46jr0.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+    try {
+        const mobileCollection = client.db('mobileResale').collection('mobiles');
+        const categoryCollection = client.db('mobileResale').collection('categories');
+
+        app.get('/mobiles', async (req, res) => {
+            const query = {};
+            const mobiles = await mobileCollection.find(query).toArray();
+            res.send(mobiles);
+
+        });
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+
 
 
 app.get('/', (req, res) => {
